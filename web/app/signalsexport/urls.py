9c684d780da4c3ac0dad909c7d8a6e548de299ad
Thijs Coenen <info@thylacine.nl>
2018-06-13 08:46:37 +0200

    SIG-36 health check

--- web/app/signalsexport/urls.py
+++ web/app/signalsexport/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include
 from django.urls import path
 from api import urls as api_urls
+from health import urls as health_urls
 
 urlpatterns = [
-    path('signals-export/', include(api_urls))
+    path('signals-export/', include(api_urls)),
+    path('health/', include(health_urls))
 ]
