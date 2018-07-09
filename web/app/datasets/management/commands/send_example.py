b901d24d1f9be86b20632903a415a71abd758347
Thijs Coenen <t.coenen@amsterdam.nl>
2018-07-09 16:46:13 +0200

    WIP connect to Sigmax (known problems)

--- web/app/datasets/management/commands/send_example.py
+++ web/app/datasets/management/commands/send_example.py
@@ -14,10 +14,15 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             'example', nargs='?', help='Which message to send 1-4', default=1) 
+        parser.add_argument(
+            '--zkn_uuid', nargs='?', help='Zaak UUID to use', default='')
+        parser.add_argument(
+            '--doc_uuid', nargs='?', help='Document UUID to use', default='')
 
     def handle(self, *args, **options):
         self.stdout.write('Sending a message to Sigmax.')
-        r = send_example(options['example'])
+        self.stdout.write(str(options))
+        r = send_example(**options)
         self.stdout.write('response status code: {}'.format(r.status_code))
         self.stdout.write('Logging response.text :')
         self.stdout.write(r.text)
