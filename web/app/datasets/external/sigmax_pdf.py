4b15bd518d239b6778724c4c6a4ac4cc4b8e772e
Thijs Coenen <t.coenen@amsterdam.nl>
2018-07-12 09:42:52 +0200

    busy

---
+++ web/app/datasets/external/sigmax_pdf.py
@@ -0,0 +1,9 @@
+"""
+This module contains the PDF generation code used for Sigmax.
+"""
+
+def _generate_pdf(signal):
+    """
+    Generate PDF to send to VoegZaakdocumentToe_Lk01
+    """
+    pass
